04db8f5baf13c856dbf8bd283413a9d78a54b94f
Nour <nouramer725@gmail.com>
2024-08-01 17:58:17 +0300

    Updated Main

--- lib/main.dart
+++ lib/main.dart
@@ -52,3 +52,65 @@ class MyApp extends StatelessWidget {
 //   await Future.delayed(Duration(seconds: 2));
 //   return "Data fetched!";
 // }
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("My Flutter App"),
+//         ),
+//         body: Center(
+//           child: Text("Hello, Flutter!"),
+//         ),
+//       ),
+//     );
+//   }
+// }import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("My Flutter App"),
+//         ),
+//         body: Center(
+//           child: Text("Hello, Flutter!"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text("My Flutter App"),
+//         ),
+//         body: Center(
+//           child: Text("Hello, Flutter!"),
+//         ),
+//       ),
+//     );
+//   }
+// }
